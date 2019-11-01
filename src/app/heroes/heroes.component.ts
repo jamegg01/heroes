9b7342ec94a26c62ed23c43da7501c21227477c3
Jurin <jurin37@hotmail.co.th>
2019-11-01 16:11:52 +0700

    เสร็จตอนที่ 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -9,13 +9,20 @@ import { HEROES } from './mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-  constructor() { }
+  heroes: Hero[];
+  constructor(private heroService: HeroService) { }
+
 
   ngOnInit() {
+    this.getHeroes();
   }
+  
   selectedHero: Hero;
 onSelect(hero: Hero){
   this.selectedHero = hero;
+}
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
 }
 }
